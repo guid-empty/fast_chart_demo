bf5c971304e76a90ed53851f35208c1fbd76e721
andrey.smirnov <guid.empty@gmail.com>
2021-08-06 14:32:18 +0300

    some notes about repaint boundary - demo to show the boundaries, hash codes of isRepaintBoundary Render Objects

--- lib/main_repaint_boundary.dart
+++ lib/main_repaint_boundary.dart
@@ -1,95 +1,257 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 
+bool debugShowRepaintBoundaries = true;
 void main() {
   ///
   /// Causes each Layer to paint a box around its bounds.
   ///
   debugPaintLayerBordersEnabled = true;
+  debugRepaintRainbowEnabled = true;
 
   runApp(MyApp());
+}
+
+class RepaintBoundaryInfo extends StatelessWidget {
+  final Widget child;
+  final String description;
+  const RepaintBoundaryInfo({Key? key, required this.child, required this.description})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    if (context is StatelessElement && debugShowRepaintBoundaries) {
+      RenderObject? target = context.renderObject?.parent as RenderObject?;
+      if (target != null) {
+        while (!(target?.isRepaintBoundary ?? false)) {
+          target = target?.parent as RenderObject?;
+          if (target == null) break;
+        }
+      }
+      return target != null
+          ? Stack(
+              children: [
+                child,
+                Container(
+                  color: Colors.black,
+                  child: Text(
+                    target.hashCode.toString(),
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(fontSize: 10, color: Colors.white),
+                  ),
+                ),
+              ],
+            )
+          : child;
+    }
+
+    return child;
+  }
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            ///
-            /// trigger to repaint all painting layer
-            ///
-            OutlinedButton(
-              onPressed: () {},
-              child: Text(
-                'Press me',
-                textDirection: TextDirection.ltr,
-              ),
-            ),
-
-            //// uncomment this
-            ///
-            /// CircularProgressIndicator(),
-            ///
-            RepaintBoundary(
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Container(
-                      color: Colors.redAccent,
+      home: Scaffold(
+        body: RepaintBoundaryInfo(
+          description: 'Center',
+          child: Center(
+            child: RepaintBoundaryInfo(
+              description: 'Column',
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  RepaintBoundaryInfo(
+                    description: 'Container',
+                    child: Container(
                       width: 200,
                       height: 100,
-                      alignment: Alignment.center,
-                      child: CustomPaint(
-                        painter: MyCustomPainter(),
+                      color: Colors.green,
+                      child: Align(
+                        alignment: Alignment.center,
                         child: Text(
-                          'Center',
+                          'Simple Container',
                           textDirection: TextDirection.ltr,
                         ),
                       ),
                     ),
-                    Container(
-                      color: Colors.orangeAccent,
-                      width: 200,
-                      height: 100,
-                      alignment: Alignment.center,
-                      child: CustomPaint(
-                        painter: MyCustomPainter(),
-                        child: Text(
-                          'Center',
-                          textDirection: TextDirection.ltr,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-
-            ///
-            /// custom painter without repaint boundary
-            ///
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                color: Colors.transparent,
-                width: 200,
-                height: 100,
-                alignment: Alignment.center,
-                child: CustomPaint(
-                  painter: MyOutOfRepaintBoundaryCustomPainter(),
-                  child: Text(
-                    'Center',
-                    textDirection: TextDirection.ltr,
                   ),
-                ),
+                  RepaintBoundaryInfo(
+                    description: 'PhysicalModel',
+                    child: PhysicalModel(
+                      color: Colors.transparent,
+                      child: RepaintBoundaryInfo(
+                        description: 'Physical Simple Text',
+                        child: Padding(
+                          padding: const EdgeInsets.all(20.0),
+                          child: Text(
+                            'Physical Simple Text',
+                            textDirection: TextDirection.ltr,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  RepaintBoundaryInfo(
+                    description: 'Simple Text',
+                    child: Container(
+                      child: Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: Text(
+                          'Simple Text',
+                          textDirection: TextDirection.ltr,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Chip(
+                    label: RepaintBoundaryInfo(
+                      description: 'Chip',
+                      child: Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: Text(
+                          'Chip',
+                          textDirection: TextDirection.ltr,
+                        ),
+                      ),
+                    ),
+                  ),
+
+                  ///
+                  /// trigger to repaint all painting layer
+                  ///
+                  RepaintBoundaryInfo(
+                    description: 'OutlinedButton',
+                    child: OutlinedButton(
+                      onPressed: () {},
+                      child: Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: Text(
+                          'Press me',
+                          textDirection: TextDirection.ltr,
+                        ),
+                      ),
+                    ),
+                  ),
+                  // CircularProgressIndicator(),
+
+                  //// uncomment this
+                  ///
+                  /// CircularProgressIndicator(),
+                  ///
+                  RepaintBoundaryInfo(
+                    description: 'RepaintBoundary',
+                    child: RepaintBoundary(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Container(
+                              color: Colors.redAccent,
+                              width: 200,
+                              height: 100,
+                              alignment: Alignment.center,
+                              child: RepaintBoundaryInfo(
+                                description: 'Painting  0',
+                                child: SizedBox(
+                                  width: 120,
+                                  height: 40,
+                                  child: CustomPaint(
+                                    painter: MyCustomPainter(0),
+                                    child: Center(
+                                      child: Text(
+                                        'Painting  0',
+                                        textDirection: TextDirection.ltr,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Container(
+                              color: Colors.orangeAccent,
+                              width: 200,
+                              height: 100,
+                              alignment: Alignment.center,
+                              child: RepaintBoundaryInfo(
+                                description: 'Painting  1',
+                                child: SizedBox(
+                                  width: 120,
+                                  height: 40,
+                                  child: CustomPaint(
+                                    painter: MyCustomPainter(1),
+                                    child: Center(
+                                      child: Text(
+                                        'Painting  1',
+                                        textDirection: TextDirection.ltr,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+
+                  ///
+                  /// custom painter without repaint boundary
+                  ///
+                  PhysicalModel(
+                    color: Colors.transparent,
+                    child: Container(
+                      color: Colors.transparent,
+                      width: 200,
+                      height: 100,
+                      alignment: Alignment.center,
+                      child: RepaintBoundaryInfo(
+                        description: 'Painting  2',
+                        child: SizedBox(
+                          width: 120,
+                          height: 40,
+                          child: CustomPaint(
+                            painter: MyCustomPainter(2),
+                            child: Center(
+                              child: Text(
+                                'Painting  2',
+                                textDirection: TextDirection.ltr,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    color: Colors.transparent,
+                    width: 200,
+                    height: 100,
+                    alignment: Alignment.center,
+                    child: RepaintBoundaryInfo(
+                      description: 'Painting  3',
+                      child: SizedBox(
+                        width: 120,
+                        height: 40,
+                        child: CustomPaint(
+                          painter: MyCustomPainter(3),
+                          child: Center(
+                            child: Text(
+                              'Painting  3',
+                              textDirection: TextDirection.ltr,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
-          ],
+          ),
         ),
       ),
     );
@@ -97,21 +259,13 @@ class MyApp extends StatelessWidget {
 }
 
 class MyCustomPainter extends CustomPainter {
-  @override
-  void paint(Canvas canvas, Size size) {
-    print('painting logic');
-    canvas.clipRect(Offset.zero & size);
-    canvas.drawColor(Colors.blueAccent, BlendMode.srcOver);
-  }
+  final int index;
+
+  MyCustomPainter(this.index);
 
   @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) => true;
-}
-
-class MyOutOfRepaintBoundaryCustomPainter extends CustomPainter {
-  @override
   void paint(Canvas canvas, Size size) {
-    print('out of repaint boundary painting logic');
+    print('painting logic $index');
     canvas.clipRect(Offset.zero & size);
     canvas.drawColor(Colors.blueAccent, BlendMode.srcOver);
   }
